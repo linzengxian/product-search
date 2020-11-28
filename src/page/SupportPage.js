a8a202a4e16432b9faa1c91c2fafddef3bfe560c
shengzhizhou <40223156+shengzhizhou@users.noreply.github.com>
2020-11-28 13:54:59 -0500

    Update SupportPage.js

--- src/page/SupportPage.js
+++ src/page/SupportPage.js
@@ -27,17 +27,51 @@ class SupportPage extends Component {
               <Title level={1} >ProductFinder Support</Title>
                 <Title id='How-to-search-a-product' level={2}>How to search a product</Title>
                 <Title level={5}>
-                You can click on left side button to upload an image, then this application will automatically search for similar products and display products information on right side in order of similarity.
+                You can click on left side button to upload an image, then our application will automatically search for similar products and display products information on right side in order of similarity.
                 </Title>
                 <Title id='How-to-know-product-details' level={2}>How to know product details</Title>
-                <Title id='Clicking-the-image' level={3}>Clicking the image</Title>
+                <Title id='Click-A-Product' level={3}>Click A Product</Title>
                 <Title level={5}>
-                  You can click on image to see more details of product, including larger image, product name, product description,
+                  You can click on image to see more details of product, including larger image, product name, product description
                 </Title>
-                <Title id='Voice-Commands' level={3}>Voice Commands</Title>
+
+                <Title id='Voice-Controll' level={3}>Voice Controll</Title>
                 <Title level={5}>
-                  You can use voice commands to controll the application speak the information to you.
+                  You can use voice commands to controll the application speaking the information to you. Please check the Voice Commands section.
                 </Title>
+
+                <Title id='Voice-Commands' level={2}>Voice Commands</Title>
+                <Title level={5}>
+                  Following are supported commands to controll the application to speak out informations. Please say complete sentence to describe your questions.
+                </Title>
+                <Title id="Alex" level={3}>Alex</Title>
+                <Title level={5}>
+                   Say "Alex" to start Speech Recognition
+                </Title>
+
+                <Title id="Ask-Product-Information" level={3}>Ask Product Information</Title>
+                <Title level={4}>when at homepage</Title>
+                <Title level={5}>
+                   Say "What is first product" to let the application speak out first product's name and description. This command also apply to second, third, etc.
+                </Title>
+                <Title level={5}>
+                   Say "What is price of first product" to let the application speak out first product's price. This command also apply to second, third, etc.
+                </Title>
+
+                <Title level={4}>After Clicking Specific Product</Title>
+                <Title level={5}>
+                   Say "What is it" to let the application speak out current product's name and description.
+                </Title>
+                <Title level={5}>
+                   Say "What is price of this product" or "How much is this product" to let the application speak out current product's price. 
+                </Title>
+
+                <Title id="Stop" level={3}>Stop</Title>
+                <Title level={5}>
+                   Say "Stop" to stop Speech Recognition
+                </Title>
+
+
               </div>
             </Col>
             <Col span={4} offset={1}>
@@ -45,14 +79,22 @@ class SupportPage extends Component {
                 <Anchor style={{marginTop:'20px'}}>
                   <Link href="#How-to-search-a-product" title="How to search a product" ></Link>
                   <Link href="#How-to-know-product-details" title="How to know product details">
-                    <Link href="#Clicking-the-image" title="Clicking the image" />
-                    <Link href="#Voice-Commands" title="Voice Commands" />
+                    <Link href="#Click-A-Product" title="Click A Product" />
+                    <Link href="#Voice-Controll" title="Voice Controll" />
+                  </Link>
+                  <Link href="#Voice-Commands" title="Voice Commands">
+                    <Link href="#Alex" title="Alex" />
+                    <Link href="#Ask-Product-Information" title="Ask Product Information" />
+                    <Link href="#Stop" title="Stop" />
                   </Link>
                 </Anchor>
               </div>
             </Col>
           </Row>
         </Layout>
+        <Footer style={{height:'500px',backgroundColor: 'white'}}></Footer>
+        
+        
       </Layout>
     );
   }
